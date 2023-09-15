db5f4ee4aebf6e14a513a7eae62f2c549cef5b4d
Tom Herbers <mail@tomherbers.de>
2023-09-15 16:41:20 +0200

    Revert "prometheus: init"
    This reverts commit fe23ecc23f280e7d69b806796317ecb289db2d7c.

--- package/uintent/files/lib/uintent/config.d/60-prometheus.lua
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/lua
-
-local uci = require("uintent.simple-uci").cursor()
-
-uci:set("prometheus-node-exporter-lua", "main", "listen_interface", "*")
-
-uci:commit("prometheus-node-exporter-lua")
