d1b6ca6e66c554cac32ddee37f60223ee8ecc457
Tom Herbers <mail@tomherbers.de>
2023-10-16 03:53:46 +0200

    network: allow configuring ipv6

--- package/uintent/files/lib/uintent/config.d/30-network.lua
+++ package/uintent/files/lib/uintent/config.d/30-network.lua
@@ -68,6 +68,68 @@ for ifname, network in pairs(profile["networks"]) do
 			end
 		end
 	end
+	if util.table_contains_key(network, "ip6") then
+		for name, address_config in pairs(network["ip6"]) do
+			local section_name = ifname .. "_" .. name .. "_6"
+			local proto = address_config["type"]
+
+			uci:section("network", "interface", section_name, {
+				ifname = bridge_name,
+				proto = proto,
+			})
+
+			uci:set("network", section_name, "metric", address_config["metric"])
+
+			if proto == "static" then
+				uci:set("network", section_name, "ip6addr", address_config["address"])
+				uci:set("network", section_name, "ip6gw", address_config["gateway"])
+				if util.table_contains_key(address_config, "dns") then
+					uci:set_list("network", section_name, "dns", address_config["dns"])
+				end
+				uci:set("network", section_name, "ip6prefix", address_config["ip6prefix"])
+				uci:set("network", section_name, "ip6hint", address_config["ip6hint"])
+				uci:set("network", section_name, "ip6ifaceid", address_config["ip6ifaceid"])
+				if util.table_contains_key(address_config, "ip6class") then
+					uci:set_list("network", section_name, "ip6class", address_config["ip6class"])
+				end
+				uci:set("network", section_name, "netmask", address_config["netmask"])
+			elseif proto == "dhcpv6" then
+				uci:set("network", section_name, "reqaddress", address_config["reqaddress"])
+				if util.table_contains_key(address_config, "reqprefix") then
+					uci:set("network", section_name, "reqprefix", address_config["reqprefix"])
+				else
+					uci:set("network", section_name, "reqprefix", "no")
+				end
+				uci:set("network", section_name, "clientid", address_config["clientid"])
+				uci:set("network", section_name, "ifaceid", address_config["ifaceid"])
+				if util.table_contains_key(address_config, "dns") then
+					uci:set_list("network", section_name, "dns", address_config["dns"])
+				end
+				uci:set("network", section_name, "peerdns", address_config["peerdns"])
+				uci:set("network", section_name, "defaultroute", address_config["defaultroute"])
+				if util.table_contains_key(address_config, "reqopts") then
+					uci:set_list("network", section_name, "reqopts", address_config["reqopts"])
+				end
+				uci:set("network", section_name, "defaultreqopts", address_config["defaultreqopts"])
+				uci:set("network", section_name, "sendopts", address_config["sendopts"])
+				uci:set("network", section_name, "noslaaconly", address_config["noslaaconly"])
+				uci:set("network", section_name, "forceprefix", address_config["forceprefix"])
+				uci:set("network", section_name, "norelease", address_config["norelease"])
+				uci:set("network", section_name, "ip6prefix", address_config["ip6prefix"])
+				uci:set("network", section_name, "sourcefilter", address_config["sourcefilter"])
+				uci:set("network", section_name, "vendorclass", address_config["vendorclass"])
+				uci:set("network", section_name, "userclass", address_config["userclass"])
+				uci:set("network", section_name, "delegate", address_config["delegate"])
+				uci:set("network", section_name, "soltimeout", address_config["soltimeout"])
+				uci:set("network", section_name, "fakeroute", address_config["fakeroute"])
+				uci:set("network", section_name, "ra_holdoff", address_config["ra_holdoff"])
+				uci:set("network", section_name, "noclientfqdn", address_config["noclientfqdn"])
+			end
+			if util.table_contains_key(address_config, "ip6assign") then
+				uci:set("network", section_name, "ip6assign", address_config["ip6assign"])
+			end
+		end
+	end
 	if util.table_contains_key(network, "ncm") then
 		local section_name = ifname .. "_" .. "ncm"
 
